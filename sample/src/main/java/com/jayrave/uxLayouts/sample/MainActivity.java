b1f7411431cd81fca9317cdfd9d25ff9436d566a
Jayanthan Raveendiran <jayanthan.raveendiran@gmail.com>
2015-11-15 17:27:44 -0800

    add basic sample app

---
+++ sample/src/main/java/com/jayrave/uxLayouts/sample/MainActivity.java
@@ -0,0 +1,23 @@
+package com.jayrave.uxLayouts.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return false;
+    }
+}
